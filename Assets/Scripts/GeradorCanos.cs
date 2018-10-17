047dbf96afe7dd278ba896506e5650c55eba18f3
Paulo Salvatore <salvatoregames@gmail.com>
2018-10-17 17:24:30 -0300

    Initial commit

---
+++ Assets/Scripts/GeradorCanos.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeradorCanos : MonoBehaviour {
+
+	public GameObject canoPrefab;
+	public float delay = 2f;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("GerarCano", delay, delay);
+	}
+
+	void GerarCano () {
+		Instantiate(canoPrefab);
+	}
+}
---
+++ Assets/Scripts/Jogador.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Jogador : MonoBehaviour {
+
+	Rigidbody2D rb;
+
+	public float forcaPulo = 200f;
+
+	int pontos = 0;
+	public Text pontosText;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Pular();
+		}
+	}
+
+	private void Pular()
+	{
+		rb.velocity = Vector2.zero;
+		rb.AddForce(Vector2.up * forcaPulo);
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.CompareTag("Canos"))
+		{
+			SceneManager.LoadScene(0);
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collider)
+	{
+		if (collider.CompareTag("ColisorPontos"))
+		{
+			pontos++;
+			pontosText.text = "Pontos: " + pontos;
+		}
+	}
+}
